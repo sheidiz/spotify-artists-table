45fe522a34710baf805e070b48fb59ebc13bfec6
Sheila Diz <dizsheila07@gmail.com>
2024-01-27 01:39:00 -0300

    feat: change home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,26 @@
 import PageContainer from "../components/PageContainer";
+import ArtistsTableGeneratorImg from "../assets/tunecards-artists-table-example.png";
 
 function Home() {
     return (
         <PageContainer>
-            <div className='text-tertiary text-center w-3/4'>
-                <h1 className='text-5xl font-bold pb-2'>Spotify Artist Table</h1>
-                <h2 className='text-4xl font-semibold'>Generator</h2>
+            <div className='lg:mx-6 mt-10 flex flex-col md:flex-row flew-wrap gap-6 md:gap-4'>
+                <div className="text-tertiary text-center md:text-start">
+                    <h1 className='px-4 lg:px-2 mb-3 text-4xl lg:text-6xl font-semibold pb-2'>
+                        Visualize Your Spotify Stats
+                    </h1>
+                    <p className='px-2 text-lg lg:text-2xl'> Ever wondered which artists would make the ultimate dinner party guest list based on your listening habits? </p>
+                    <p className='px-2 mb-3 text-lg lg:text-2xl' > Or how about your very own personalized {`"Spotify Wrapped"`} anytime of the year?</p>
+                    <p className='px-2 text-lg'>
+                        Discover and share your favorite beats in a whole new way with TuneCards - and {"it's"} completely free!
+                    </p>
+                </div>
+                <div className='text-secondary text-center grid grid-cols-2 gap-4'>
+                    <img src={ArtistsTableGeneratorImg} alt="ArtistsTableGenerator" className="mt-5" />
+                    <img src={ArtistsTableGeneratorImg} alt="ArtistsTableGenerator2" className="mb-5" />
+                </div>
             </div>
-            <div className='text-secondary text-center w-3/4'>
-                <h3 className='text-3xl font-semibold'>See which artists would be on your celebrity table!</h3>
-            </div>
-        </PageContainer>
+        </PageContainer >
     )
 }
 
